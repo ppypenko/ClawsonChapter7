6e9be81b4d3334b4abd5a9b659f19bc3c190e301
ppypenko <ppypenko@student.neumont.edu>
2016-04-26 12:41:44 -0600

    Began on sign up. (Second iteration)

--- SportsStore.WebUI/Controllers/UserController.cs
+++ SportsStore.WebUI/Controllers/UserController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SportsStore.WebUI.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,28 @@ namespace SportsStore.WebUI.Controllers
 {
     public class UserController : Controller
     {
+        SqlConnection db = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SportsStore;Integrated Security=True");
         // GET: User
         public ActionResult SignUp()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                if(CheckIfSame(user.Password, user.Password2))
+                {
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Passwords do not match!");
+                }
+            }
+            return View(user);
         }
         public ActionResult SignIn()
         {
@@ -20,6 +40,15 @@ namespace SportsStore.WebUI.Controllers
         public ActionResult SignOut()
         {
             return View();
+        }
+        private bool CheckIfExists(string name, string password)
+        {
+            //var query = from u in db.
+            return true;
+        }
+        private bool CheckIfSame(string password1, string password2)
+        {
+            return password1 == password2;
         }
     }
 }
